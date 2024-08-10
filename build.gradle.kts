c38092d7d647a0464833117e9277bc189f8018f4
Paul-Christian Volkmer <code@pcvolkmer.de>
2024-08-10 15:56:19 +0200

    build: disable native image build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 plugins {
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
-    id("org.graalvm.buildtools.native") version "0.10.2"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
     jacoco
